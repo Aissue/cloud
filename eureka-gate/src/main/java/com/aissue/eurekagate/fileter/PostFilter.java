060667c2cd155bb629749f2a2489a1974d475490
wangkun <aissue@qq.com>
2019-05-08 11:37:12 +0800

    增加gate门户zuul的过滤器

---
+++ eureka-gate/src/main/java/com/aissue/eurekagate/fileter/PostFilter.java
@@ -0,0 +1,34 @@
+package com.aissue.eurekagate.fileter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class PostFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("PostFilter...");
+        return null;
+    }
+}
---
+++ eureka-gate/src/main/java/com/aissue/eurekagate/fileter/RoutingFilter.java
@@ -0,0 +1,34 @@
+package com.aissue.eurekagate.fileter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class RoutingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "routing";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("routingFilter...");
+        return null;
+    }
+}
